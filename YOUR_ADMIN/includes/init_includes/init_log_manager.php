f8e356e7da9f711dcd887de0953cd4c22d1230ea
lat9 <lat9@vinosdefrutastropicales.com>
2017-08-07 10:50:10 -0400

    #2, #3: Use session variable, create log
    Update the processing to simply use a session-variable to note that the
    processing has run and create/update a log of the plugin's processing.

--- YOUR_ADMIN/includes/init_includes/init_log_manager.php
+++ YOUR_ADMIN/includes/init_includes/init_log_manager.php
@@ -3,6 +3,9 @@
 // Part of the Log Manager plugin, created by lat9 (lat9@vinosdefrutastropicales.com)
 // Copyright (c) 2017, Vinos de Frutas Tropicales.
 //
+// -----
+// If the plugin's configuration settings (in Configuration->Logging) haven't been set, set them now!
+//
 if (!defined('LOG_MANAGER_KEEP_DAYS')) {
     $next_sort = $db->Execute(
         "SELECT MAX(sort_order) as max_sort 
@@ -23,12 +26,20 @@ if (!defined('LOG_MANAGER_KEEP_DAYS')) {
             
             ( 'Log Manager: Logs to Keep', 'LOG_MANAGER_KEEP_THESE', 'zcInstall', 'Enter a comma-separated list of name-prefixes for any log-files that you want to <b><i>keep</i></b>, regardless of their age.<br /><br />The values you enter are <em>case-sensitive</em>, i.e. <em>zcInstall</em> is different than <em>zcinstall</em>.  The default setting (<code>zcInstall</code>) results in any file matching <code>/logs/zcInstall*.log</code> being kept regardless of its creation date.<br />', 10, $sort_order2, now(), NULL, NULL)"
     );
-} else {
-    // -----
-    // If we're not currently on the login page, but the "referring page" is the login page, continue to see if there are logs
-    // to be removed ... if so configured.
-    //
-    if ($current_page != 'login.php' && isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], zen_href_link(FILENAME_LOGIN, '', 'SSL')) === 0 && ((int)LOG_MANAGER_KEEP_DAYS) > 0) {
+    define('LOG_MANAGER_KEEP_DAYS', '0');
+    define('LOG_MANAGER_KEEP_THESE', 'zcInstall');
+}
+
+// -----
+// If the plugin is enabled and current session is associated with a logged-in admin and the plugin's processing hasn't been run
+// yet for this session ... manage those logs!
+//
+if (isset($_SESSION['admin_id'])) {
+    if (((int)LOG_MANAGER_KEEP_DAYS) > 0 && !isset($_SESSION['log_managed'])) {
+        // -----
+        // Build up a string-to-match (via preg_match) for the .log files that should be "kept".  That string
+        // contains the log-file name prefixes.
+        //
         $match_string = '';
         if (LOG_MANAGER_KEEP_THESE != '') {
             $logs_to_keep = explode(',', str_replace(' ', '', LOG_MANAGER_KEEP_THESE));
@@ -38,15 +49,31 @@ if (!defined('LOG_MANAGER_KEEP_DAYS')) {
                 $match_string = '/^' . $logs_to_keep[0] . '.*$/';
             }
         }
+        
+        // -----
+        // Determine the keep-until date (some number of days **prior to** today).
+        //
         $keep_until = strtotime('-' . LOG_MANAGER_KEEP_DAYS . ' day');
         $keep_until_date = date(DATE_FORMAT . ' H:m:i', $keep_until);
+        
+        // -----
+        // Loop through all files present in the /logs directory ...
+        //
         $files_removed = 0;
         if ($dir = dir(DIR_FS_LOGS)) {
             while (($current_file = $dir->read()) !== false) {
+                // -----
+                // ... looking for a ".log" file with a name that isn't in the "keep-these" list ...
+                //
                 if (strpos($current_file, '.log') !== false && $match_string != '' && !preg_match($match_string, $current_file)) {
+                    // -----
+                    // ... that was created prior to the keep-until date ...
+                    //
                     $modified = filemtime(DIR_FS_LOGS . DIRECTORY_SEPARATOR . $current_file);
                     if ($modified !== false && $modified < $keep_until) {
                         // -----
+                        // ... and remove it.
+                        //
                         // I'm not a big fan of inhibiting error-reports, but for this case it's possible that multiple
                         // admins have "hit" the site concurrently and are all causing this script to run.
                         //
@@ -57,8 +84,19 @@ if (!defined('LOG_MANAGER_KEEP_DAYS')) {
             }
             $dir->close();
         }
+        
+        // -----
+        // If one or more files was removed, let the admin know (via message) and log the removal action.
+        //
         if ($files_removed != 0) {
-            $messageStack->add(sprintf(LOG_MANAGER_FILES_MESSAGE_FORMAT, $files_removed, $keep_until_date), 'success');
+            $logMessage = sprintf(LOG_MANAGER_FILES_MESSAGE_FORMAT, $files_removed, $keep_until_date);
+            $messageStack->add($logMessage, 'success');
+            error_log(date(PHP_DATE_TIME_FORMAT) . ', ' . $_SESSION['admin_id'] . ": $logMessage" . PHP_EOL, 3, DIR_FS_LOGS . '/log_manager_removal.log');
         }
     }
+        
+    // -----
+    // Note that the plugin's processing has been run for the current admin session.
+    //
+    $_SESSION['log_managed'] = true;
 }
